d9637cfb1aa3f15ab8ee296f09446b5b80ff9f50
Moritz <moritz.haas@hs-furtwangen.de>
2016-11-29 16:55:31 +0100

    JUnit Testklassen hinzugefügt

--- upperbuchstabe/src/main/java/de/moritz/upperbuchstabe/App.java
+++ upperbuchstabe/src/main/java/de/moritz/upperbuchstabe/App.java
@@ -4,41 +4,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 /**
- * Hello world!
+ * Dieses Programm liest einen String kleiner Buchstaben ein und gibt ihn in
+ * Großbuchstaben wieder aus.
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	System.out.println("String eingeben: ");
-    	System.out.println(read().toUpperCase());
-        
-    }
-    
-    
-    
+public class App {
+	/**
+	 * Hauptprogramm:
+	 * 
+	 * @param args
+	 *            Kommandozeilenparameter
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("String eingeben: "); // HALLOALLOOD
+		System.out.println(read().toUpperCase());
+	}
+
+	/**
+	 * Readfunktion.
+	 * @return readname 
+	 * String 
+	 */
 	public static String read() {
+
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		String readname = "leer";
 		try {
 			readname = reader.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	return readname;
-		
+		return readname;
+
 	}
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
